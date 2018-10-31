fee91b0ea47417f31a90baa0cba82afab8014857
andychan94 <nrashidov@yahoo.com>
2018-10-31 21:14:37 +0900

    telegram username fully added

---
+++ src/Form/Extension/CustomerRegistrationTypeExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form\Extension;
+
+use Sylius\Bundle\CoreBundle\Form\Type\Customer\CustomerRegistrationType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class CustomerRegistrationTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('telegramUsername', TextType::class, [
+            'required' => false,
+            'label' => 'telegram_username',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType(): string
+    {
+        return CustomerRegistrationType::class;
+    }
+}
